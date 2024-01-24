cb47f114a3574ef9cff9f57e5a0eb51f55846a8c
paramshah1903 <shahparam1903@gmail.com>
2024-01-24 20:14:33 +0530

    prize section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Prize from "@/components/prize";
+import PrizeSection from "@/components/prizeSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Prize />
+    </main>
   );
 }
---
+++ components/prize.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PrizeSection from "./prizeSection";
+
+export default function Prize() {
+  return (
+    <div className="flex gap-0 container">
+      <PrizeSection />
+      <div className="ml-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe fuga quia
+        officia assumenda in voluptatibus cupiditate pariatur delectus facilis
+        soluta exercitationem, aspernatur eum maxime voluptates ex quas quam
+        sequi provident modi iste accusantium quos! Placeat suscipit maxime
+        quaerat aliquam doloribus incidunt voluptatum consequatur, iste,
+        expedita ut adipisci veritatis quidem veniam.
+      </div>
+    </div>
+  );
+}
