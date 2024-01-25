cd3419ed870d30adecd991f90af1e2b88341db34
paramshah1903 <shahparam1903@gmail.com>
2024-01-26 00:23:18 +0530

    team and contact section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Prize from "@/components/prize";
 import PrizeSection from "@/components/prizeSection";
+import ContactSection from "@/components/contactSection";
 import Image from "next/image";
+import TeamSection from "@/components/teamSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Prize />
+      <ContactSection />
+      <TeamSection />
     </main>
   );
 }
--- components/prize.tsx
+++ components/prize.tsx
@@ -5,13 +5,23 @@ export default function Prize() {
   return (
     <div className="flex gap-0 container">
       <PrizeSection />
-      <div className="ml-10">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe fuga quia
-        officia assumenda in voluptatibus cupiditate pariatur delectus facilis
-        soluta exercitationem, aspernatur eum maxime voluptates ex quas quam
-        sequi provident modi iste accusantium quos! Placeat suscipit maxime
-        quaerat aliquam doloribus incidunt voluptatum consequatur, iste,
-        expedita ut adipisci veritatis quidem veniam.
+      <div className="ml-8 mb-10">
+        <p>
+          Elevate your hackathon experience with our exclusive Shark Tank event!
+          Five prominent industry leaders, each a seasoned entrepreneur and
+          investor, will be joining us to pitch their unique problem statements.
+          Participants will have the extraordinary opportunity to showcase their
+          innovative solutions directly to these Shark Tank pitchers.
+        </p>
+        <p>
+          Each Shark Tank judge will carefully evaluate the presented solutions
+          and select one outstanding team whose project aligns most closely with
+          their problem statement. The winning teams will not only gain
+          invaluable insights from industry experts but will also be awarded
+          exclusive prizes directly chosen by the Shark Tank pitchers
+          themselves. Don't miss the chance to impress these titans of industry
+          and take home a prize that could propel your project to new heights!
+        </p>
       </div>
     </div>
   );
