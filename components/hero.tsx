71a652905099902fd6a16613aa411f0e53a0de75
pragneshbarik <barikpragnesh@gmail.com>
2024-02-17 19:25:36 +0530

    added devfolio button

--- components/hero.tsx
+++ components/hero.tsx
@@ -14,12 +14,23 @@ import background from "@/assets/images/Clip path group.svg";
 import sharks from "@/assets/images/sharks.svg";
 import sharks2 from "@/assets/images/sharks 2.svg";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+import { useEffect } from "react";
 
 const azonix = localFont({ src: "../assets/fonts/Azonix.otf" });
 
 export default function Hero() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
-    <ParallaxProvider>
+    <>
       <div className="h-[calc(100dvh-60px)] w-11/12 mx-auto flex items-center justify-center ">
         <img
           src={background.src}
@@ -34,48 +45,47 @@ export default function Hero() {
           className="absolute top-50 object-cover h-screen w-full -z-40 lg:hidden"
         />
         <div className="z-10">
-          <Parallax speed={5}>
-            <div
-              className={
-                "text-[#FF5003] font-bold flex flex-col justify-center " +
-                azonix.className
-              }
-            >
-              <div className="md:text-9xl text-7xl text-center">Hack</div>
-              <div className="md:text-9xl text-7xl flex gap-10 text-center items-center">
-                <div>The</div>
-                <div className="md:text-7xl text-5xl">2.0</div>
-              </div>
-              <div className="md:text-9xl text-7xl  text-center">Tank </div>
+          <div
+            className={
+              "text-[#FF5003] font-bold flex flex-col justify-center " +
+              azonix.className
+            }
+          >
+            <div className="md:text-9xl text-7xl text-center">Hack</div>
+            <div className="md:text-9xl text-7xl flex gap-10 text-center items-center">
+              <div>The</div>
+              <div className="md:text-7xl text-5xl">2.0</div>
             </div>
-          </Parallax>
-          <Parallax speed={-2}>
-            <p
-              className={
-                "pl-4 md:text-4xl text-2xl text-center pt-2  " +
-                azonix.className
-              }
-            >
-              Dare to dive in
-            </p>
-          </Parallax>
-          <div className="flex mt-5 items-center justify-center">
-            <div className="z-10">
-              <Button variant="outline" className="bg-[#0044D1] h-[48px]">
-                <DiscordLogoIcon className="mr-2 " />
-                Discord
-              </Button>
+            <div className="md:text-9xl text-7xl  text-center">Tank </div>
+          </div>
+
+          <p
+            className={
+              "pl-4 md:text-4xl text-2xl text-center pt-2  " + azonix.className
+            }
+          >
+            Dare to dive in
+          </p>
+
+          <div className=" mt-5 items-center justify-center">
+            <div className="flex flex-col gap-2 z-10 rounded items-center">
+              <div
+                className="apply-button"
+                data-hackathon-slug="hack-the-tank-2"
+                data-button-theme="dark"
+                style={{ height: "44px", width: "312px" }}
+              ></div>
               <Button
-                variant="secondary"
-                className="ml-2 bg-[#FF5003] h-[48px] "
+                variant="outline"
+                className="bg-[#0044D1] h-[44px] w-[320px] rounded-none"
               >
-                <EnterIcon className="mr-2" />
-                Register Now
+                <DiscordLogoIcon className="mr-2" />
+                Discord
               </Button>
             </div>
           </div>
         </div>
       </div>
-    </ParallaxProvider>
+    </>
   );
 }
--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -7,15 +7,15 @@ export default function Sponsors() {
       <div className=" px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
-            Our Sponsers
+            Our Sponsors
           </h1>
-          {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
             gentrify, subway tile poke farm-to-table. Franzen you probably
             haven&apos;t heard of them.
-          </p> */}
+          </p>
         </div>
-        {/* <div className="flex flex-wrap -m-2">
+        <div className="flex flex-wrap -m-2">
           <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
             <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
               <img
@@ -149,12 +149,6 @@ export default function Sponsors() {
               </div>
             </div>
           </div>
-        </div> */}
-        <div className={
-                "md:text-6xl text-4xl text-center text-gray-400 " +
-                azonix.className
-              }>
-          Announcing Soon
         </div>
       </div>
     </section>
